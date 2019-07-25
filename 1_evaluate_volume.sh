2c6e0252b48927fb23d366abed380c98a1085b26
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2019-07-25 14:52:34 -0500

    Adding count of permission denied errors to output

--- 1_evaluate_volume.sh
+++ 1_evaluate_volume.sh
@@ -15,3 +15,7 @@ echo Writing to $OUT
 echo Logs to $LOGERR and $LOGOUT
 bash src/evaluate_fs.sh $@ -o $OUT $VOLUME > $LOGOUT 2> $LOGERR
 
+NERR=$(grep "Permission denied" $LOGERR | wc -l)
+if [[ "$NERR" != "0" ]]; then
+    echo Note: $NERR counts of \"Permission denied\" in error log
+fi
