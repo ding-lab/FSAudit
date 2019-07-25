a26e128d8da8f5be871670f86b1e89c4ce362ef9
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2019-07-25 13:14:21 -0500

    Adding script to run all steps at once

---
+++ A_process_all.sh
@@ -0,0 +1,26 @@
+# Run steps 1-4, check for failure after each.
+# Modeled after https://github.com/ding-lab/discover.CPTAC3.b1/blob/master/1_process_all.sh
+source FSAudit.config
+
+NOW=$(date)
+>&2 echo [ $NOW ] Running 1_evaluate_volume.sh
+bash ./1_evaluate_volume.sh
+test_exit_status
+
+NOW=$(date)
+>&2 echo [ $NOW ] Running 2_process_stats.sh
+bash ./2_process_stats.sh
+test_exit_status
+
+NOW=$(date)
+>&2 echo [ $NOW ] Running 3_summarize_stats.sh
+bash ./3_summarize_stats.sh
+test_exit_status
+
+NOW=$(date)
+>&2 echo [ $NOW ] Running 4_plot_stats.sh
+bash ./4_plot_stats.sh
+test_exit_status
+
+NOW=$(date)
+>&2 echo [ $NOW ] Completed successfully
