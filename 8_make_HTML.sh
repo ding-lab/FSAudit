a3b73db073b9076a77e952a35c709878f01f9152
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2025-01-30 18:27:24 -0600

    continued development

--- 8_make_HTML.sh
+++ 8_make_HTML.sh
@@ -1,18 +1,25 @@
-# 1000 dev data
-DAT="dat/m.wyczalkowski.1000.dirmap2.tsv"
-OUT="html/test-1000.html"
-X="dat/none.dat"
-LIM=0
-CAT="cat"
-COL="3"     # this is the one with size labels
+#DATA="1000dev"
+DAT="mw"  # full m.wyczalkowski dataset
 
-# real data
-#DAT="dat/m.wyczalkowski.20250121.dirmap2.tsv.gz"
-#OUT="html/test-1000.html"
-#X="dat/exclude.dat"
-#LIM=1000000000 # 1G
-#CAT="gzcat"
-#COL="3"
+
+#if [[ "$DATA" == "1000dev" ]]; 
+#echo x
+    # 1000 dev data
+#    DAT="dat/m.wyczalkowski.1000.dirmap2.tsv"
+#    OUT="html/test-1000.html"
+#    X="dat/none.dat"
+#    LIM=0
+#    CAT="cat"
+#    COL="3"     # this is the one with size labels
+##elif [[ $DATA == "mw" ]]; 
+#    # real data
+    DAT="dat/m.wyczalkowski.20250121.dirmap3.tsv.gz"
+    OUT="html/m.wyczalkowski.20250121.html"
+    X="dat/exclude.dat"
+    LIM=1000000000 # 1G
+    CAT="gzcat"
+    COL="3"
+#fi
 
 $CAT $DAT | awk -v LIM=$LIM 'BEGIN{FS="\t"; OFS="\t"}{if ($2 > LIM) print}' | cut -f 3 |  grep -vf $X | dirtree -o $OUT
 
