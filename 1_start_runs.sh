3c88b11bca7f339b91617f910a6ca8eb88a28179
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2025-11-03 13:53:14 -0600

    Safety check to avoid overwriting raw file

--- 1_start_runs.sh
+++ 1_start_runs.sh
@@ -8,6 +8,11 @@ mkdir -p $OUTD
 
 OUTFN="$OUTD/$RUN_NAME.rawstat.gz"
 
+if [ -e $OUTFN ]; then
+    >&2 echo ERROR: $OUTFN exists.  Delete if necessary
+    exit 1
+fi
+
 >&2 echo Finding all files in $VOL_PATH
 >&2 echo Writing to $OUTFN
 
