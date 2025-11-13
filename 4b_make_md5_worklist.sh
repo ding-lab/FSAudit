100701ece0209ed31d0eba6fe52b63458c8d1fb9
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2025-11-13 12:45:02 -0600

    MD5 generation on katmai

--- 4b_make_md5_worklist.sh
+++ 4b_make_md5_worklist.sh
@@ -1,16 +1,20 @@
+source config.sh
 
-#FILELIST=/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/dinglab.20250909/dinglab.20250909.filelist.tsv.gz
-#PAST_MD5=/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/dinglab.20250728/dinglab.20250728.filelist.gt_1Gb_md5.tsv
+ALT_RUN_NAME="katmai.20251103b"
+RUN_NAME=$ALT_RUN_NAME
 
-# Switching to mwyczalkowski for manageability
-#FILELIST="/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/mwyczalkowski.20250815/mwyczalkowski.20250815.filelist.tsv.gz"
-#PAST_MD5=/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/m.wyczalkowski.20250612/m.wyczalkowski.20250612.filelist.gt_1Gb_md5.tsv
+FILELIST="$OUTD/${RUN_NAME}.filelist.tsv.gz"
 
-FILELIST=/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/dev/FSAudit-md5.dev-1000.filelist.tsv
-PAST_MD5=/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/dev/FSAudit-md5.dev-1000.cached-filelist-md5.tsv
+if [ ! -e $PAST_MD5 ]; then
+    >&2 echo ERROR: PAST_MD5 $PAST_MD5 does not exist
+    exit 1
+fi
 
-OUTD="/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/dev/FSAudit-md5"
-OUT="$OUTD/md5-worklist.tsv"
+#OUTD="/storage1/fs1/m.wyczalkowski/Active/ProjectStorage/Analysis/FSAudit/dev/FSAudit-md5"
+OUT="$OUTD/${RUN_NAME}.md5-worklist.tsv"
 
-python3 src/make_md5_worklist.py -m $PAST_MD5 -o $OUT $FILELIST
+CMD="python3 src/make_md5_worklist.py -m $PAST_MD5 -o $OUT $FILELIST"
 
+>&2 echo RUNNING: $CMD
+
+eval $CMD
--- 4c_launch_md5_parallel.sh
+++ 4c_launch_md5_parallel.sh
@@ -1,12 +1,30 @@
-OUTD="/home/mwyczalk_test/Projects/DataTracking/FSAudit/FSAudit2025/output/katmai.20250916"
-LIST="$OUTD/katmai.20250916.md5-worklist.tsv.gz"
+# Launch md5 calculations on lab cluster
+# uses xargs -P for parallelization
 
-OUT="$OUTD/katmai.20250916.md5-raw.tsv"
+# Suggest running this as root in tmux
+
+source config.sh
+
+ALT_RUN_NAME="katmai.20251103b"
+RUN_NAME=$ALT_RUN_NAME
+
+LIST="$OUTD/${RUN_NAME}.md5-worklist.tsv"
+OUT="$OUTD/${RUN_NAME}.md5-raw.txt"
+LOG="logs/${RUN_NAME}.md5-raw.log"
+
+N_PARALLEL=10
 
 >&2 echo Reading $LIST
 >&2 echo Writing $OUT
-date
-
-zcat $LIST | tail -n +2 | cut -f 1 | xargs -I "{}" -n 1 -P 5 md5sum "{}" > $OUT
+>&2 echo Logging $LOG
 
 date
+
+cat $LIST | tail -n +2 | cut -f 1 | xargs -I "{}" -n 1 -P $N_PARALLEL md5sum "{}" > $OUT 2> $LOG
+
+date
+
+# Note that this needs additional processing
+# something like
+# $ sed 's/  /\t/' katmai.20250916.md5-raw.tsv | gzip > katmai.20250916.md5.tsv.gz
+# but also add header: "md5", "file_name"
